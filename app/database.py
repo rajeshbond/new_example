c4f6059c9b22a319294819c80f60992e3349ac38
rajesh <rajesh.sntsolutions@gmail.com>
2022-10-17 20:55:44 +0530

    test shot

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from psycopg2.extras import RealDictConnection
-from psycopg2 import *
+import psycopg2
 from .config import settings
 
 
