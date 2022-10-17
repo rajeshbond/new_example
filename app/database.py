53a3e3b2287952de6f0c86b886748f4fbf11f092
rajesh <rajesh.sntsolutions@gmail.com>
2022-10-17 20:33:33 +0530

    change

--- app/database.py
+++ app/database.py
@@ -1,8 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-# import "psycopg2"
-# from psycopg2.extras import RealDictCursor
+from psycopg2.extras import *
 from .config import settings
 
 
