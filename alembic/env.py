f28343baa3390f0d00df27ec42b0a91b1e04762f
rajesh <rajesh.sntsolutions@gmail.com>
2022-10-19 00:07:11 +0530

    fesh database

--- alembic/env.py
+++ alembic/env.py
@@ -1,17 +1,16 @@
 from logging.config import fileConfig
 from app.config import settings
+from app.models import Base
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
-from app.models import Base
 
 from alembic import context
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-config.set_main_option("sqlalchemy.url", 
-                       f"postgresql+psycopg2://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"  # This is with env varible settings
-)
+config.set_main_option("sqlalchemy.url",
+                       f"postgresql+psycopg2://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}")
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
--- app/models.py
+++ app/models.py
@@ -8,34 +8,32 @@ from .database import Base #added
 
 
 class Post(Base):
-    __tablename__ = "posts1"
-    id = Column(Integer, primary_key=True, nullable = False)
-    title = Column(String,nullable = False)
-    content = Column(String,nullable = False)
-    publish = Column(Boolean, server_default = "True", nullable = False)
-    created_at = Column(TIMESTAMP(timezone=True), nullable = False,
-                        server_default = text('now()') )     # need to import text for SQL express 
-    owner_id = Column(Integer, ForeignKey("users.id", ondelete = "CASCADE"),nullable = False)
+    __tablename__ = "posts"
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    title = Column(String, nullable=False)
+    content = Column(String, nullable=False)
+    published = Column(Boolean, server_default='TRUE', nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True),
+                        nullable=False, server_default=text('now()'))
+    owner_id = Column(Integer, ForeignKey(
+        "users.id", ondelete="CASCADE"), nullable=False)
+
     owner = relationship("User")
-    
-       
+
+
 class User(Base):
-    __tablename__ = "users" 
-    id = Column(Integer, primary_key=True, nullable = False)
-    email = Column(String,nullable = False, unique = True)
-    password = Column(String,nullable = False)
-    created_at = Column(TIMESTAMP(timezone=True), nullable = False,
-                        server_default = text('now()') ) 
-    phone = Column(String)
-    # need to import text for SQL express 
-    
- 
-class Votes(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, nullable=False)
+    email = Column(String, nullable=False, unique=True)
+    password = Column(String, nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True),
+                        nullable=False, server_default=text('now()'))
+
+
+class Vote(Base):
     __tablename__ = "votes"
-    post_id = Column(Integer,
-                     ForeignKey("posts1.id",ondelete="CASCADE"),
-                     primary_key = True)
-    user_id = Column(Integer,
-                     ForeignKey("users.id",ondelete="CASCADE"),
-                     primary_key = True)
-    
+    user_id = Column(Integer, ForeignKey(
+        "users.id", ondelete="CASCADE"), primary_key=True)
+    post_id = Column(Integer, ForeignKey(
+        "posts.id", ondelete="CASCADE"), primary_key=True)
